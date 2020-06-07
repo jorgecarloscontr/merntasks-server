5e276105ad486cc1d9ecee84129e3cc5132b3fc2
Jorge Carlos Alvarado Contreras <jorge_carloscontr@hotmail.com>
2020-06-07 12:36:39 -0500

    setting to deployment

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 //Habilitar express json
 app.use(express.json({extended : true}));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //import routes 
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -21,6 +21,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
